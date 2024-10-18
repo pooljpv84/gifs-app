3059fe4338e9ca23d51f3df9ab05cda377640c2c
paul <pooljpv84@hotmail.com>
2024-10-18 18:41:26 -0500

    servicio obj

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {GifsService} from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -16,11 +17,13 @@ export class SearchBoxComponent
 {
   @ViewChild('txtTagInput')
   tagInput!: ElementRef<HTMLInputElement>;
-  constructor() {  }
+  constructor(private gifsService: GifsService) {  }
 
   searchTag( )
   {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag});
+    this.gifsService.searchTag(newTag);
+    //limpiar txt
+    this.tagInput.nativeElement.value = '';
   }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GifsService} from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private _gifsService: GifsService) {
+  }
+
+  get tags(): string[] {
+    return this._gifsService.tagsHistory;
+  }
 }
